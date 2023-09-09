cb8170a658a43b2111ce5782cd8b2842c940bdf3
AliGZN <geramiali.ag@gmail.com>
2023-09-09 16:39:38 +0330

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double[] a = new double[5];
+
+        for (int i = 0; i < a.length; i++) {
+
+            a[i] = 1.0;
+        }
+
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+
+    }
+}
